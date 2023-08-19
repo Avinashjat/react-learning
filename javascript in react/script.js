ca94221789de444a64ffeda927eeb73b9c2d8a83
Avinash Jat <dev.avinashjat@gmail.com>
2023-08-19 15:55:14 +0530

    basic og react

---
+++ javascript in react/script.js
@@ -0,0 +1,17 @@
+
+var a = 'i am avi'
+var a = 54;
+var a = 24;
+console.log(a)
+
+let obj1 ={
+    d:'avi',
+    b:"kuraj",
+    p:66,
+    func: function newfun(number) {
+        console.log("this is the number"+ number )
+        
+    }
+
+
+}
